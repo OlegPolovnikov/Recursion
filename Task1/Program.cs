03f6f61b08f3c73ac7ad8ef4b2ede6535ecec0ad
Oleg_Polovnikov <polovnikov@list.ru>
2024-06-28 23:41:33 +0400

    Добавили код для решения Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void OutputRange(int m, int n) 
+{
+if (n > m + 1)
+{
+    OutputRange(m, n - 1);
+    Console.Write(n - 1 + " ");    
+}
+}
+Console.WriteLine("Введите значение нижней границы m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение верхней границы n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Натуральные числа в промежутке от m до n: ");
+OutputRange(m, n);
