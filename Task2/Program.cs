00f4a212de3ad43d34950c421f2801ca41ae92c7
Oleg_Polovnikov <polovnikov@list.ru>
2024-06-29 12:42:38 +0400

    Добавили код для решения Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int A(int n, int m) 
+{
+    if (n == 0) return m + 1;
+    else if ((n != 0) && (m == 0)) return A(n - 1, 1);
+    else return A(n - 1, A(n, m - 1));
+}
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("A = " + A(n, m));
